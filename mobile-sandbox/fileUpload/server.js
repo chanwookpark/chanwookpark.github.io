d2e13a6846958a3b23e25d15e5e2073924698c69
chanwook <chanwook.god@gmail.com>
2014-01-22 13:25:02 +0900

    잘못된 form input id 수정함 등

--- mobile-sandbox/fileUpload/server.js
+++ mobile-sandbox/fileUpload/server.js
@@ -13,19 +13,24 @@ http.createServer(function (req, res) {
 
 		// from config
 		form.encoding = 'utf-8';
-		form.uploadDir = "/tmp/upload";
+		//form.uploadDir = "/tmp/upload";
 		form.type = true;
 
 		form.parse(req, function(err, fields, files){
-			res.writeHead(200, {'content-type': 'text/plain'});
-			res.write('received uplod: ');
-			res.end(util.inspect({fields: fields, files: files}));
-			
-			console.log("Upload File>> ");
+
+			console.log("Upload File! ");
+			console.log("Headers: " + JSON.stringify(req.headers));
+			/*
 			for( var i = 0 ; i < files.length ; i++) {
 				var f = files[i];
 				console.log(f.toJSON());
 			}
+			*/
+
+			// Response
+			res.writeHead(200, {'content-type': 'text/plain'});
+			res.write('received uplod: ');
+			res.end(util.inspect({fields: fields, files: files}));
 		});
 
 		return;
